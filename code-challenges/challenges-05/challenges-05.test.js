3c0612792689f699cac4ae196f66a7ce33d070d0
Leaundrae Mckinney <pytechdrae@gmail.com>
2021-02-20 10:35:15 -0500

    Solved challenge 11

--- code-challenges/challenges-05/challenges-05.test.js
+++ code-challenges/challenges-05/challenges-05.test.js
@@ -263,9 +263,12 @@ Write a function named totalSumCSV that, given a string of comma-separated value
 const totalSumCSV = (str) => {
   let total = 0;
   // Solution code here...
+  let tempArr = str.split(',')  ;
+  for(let i=0;i<tempArr.length;i++){
+    total += Number(tempArr[i]);
+  }
   return total;
 };
-
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 12 - Stretch Goal
@@ -390,7 +393,7 @@ describe('Testing challenge 10', () => {
   });
 });
 
-xdescribe('Testing challenge 11', () => {
+describe('Testing challenge 11', () => {
   test('It should add up the numbers contained within the string', () => {
     expect(totalSumCSV('1,4,5,7,2')).toStrictEqual(19);
     expect(totalSumCSV('147')).toStrictEqual(147);
